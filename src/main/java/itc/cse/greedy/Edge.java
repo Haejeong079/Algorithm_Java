09c693e2ce76a950bc247ecb9a400ce0f885f199
Haejeong079 <goesed7582@naver.com>
2024-11-14 13:08:04 +0900

    greedy 11/14

--- src/main/java/itc/cse/greedy/Edge.java
+++ src/main/java/itc/cse/greedy/Edge.java
@@ -1,2 +1,14 @@
-package itc.cse.greedy;public class Edge {
+package itc.cse.greedy;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Edge {
+    private int v1;
+    private int v2;
+    private int weight;
+
 }
--- src/main/java/itc/cse/greedy/PrimeMST.java
+++ src/main/java/itc/cse/greedy/PrimeMST.java
@@ -1,49 +1,71 @@
 package itc.cse.greedy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeMST {
-    public static final int INF=999;
-    public static void main(String[] args) {
-        PrimeMST pm=new PrimeMST();
-        int[][] G={
-                {0,2,INF,INF,4},
-                {2,0,8,INF,4},
-                {INF,8,0,7,6},
-                {INF,INF,7,0,3},
-                {4,4,6,3,0}
-        };
-        pm.mst(G);
-    }
+    public static final int INF = 999;
+
 
     public void mst(int[][] W) {
-        int n =W.length;
-        int []near=new int [n];
-        boolean[]isBlue=new boolean[n];
-        isBlue[0]=false; //적색
+        int n = W.length;
+        int[] near = new int[n];
+        boolean[] isBlue = new boolean[n];
 
-        for (int i = 0; i < n; i++) {
-            isBlue[i]=true;
-            near[i]=0;
-        }
-        int newRed=0;
-        for (int i = 0; i < n; i++) {
-            int minVal=INF;
-            for (int b = 0; b < n; b++) {
-                if(isBlue[b]&& W[b][near[b]]<minVal){
-                    minVal=W[b][near[b]];
-                    newRed=b;
-                }
-            }
-            isBlue[newRed]=false;
-            System.out.println((char)(near[newRed]+'a')+" - "+(char)(newRed+'a')+" : "+minVal);
+        isBlue[0] = false;  // 적색
 
-            for (int b = 0; b < n; b++) {
-                if(isBlue[b]&&W[b][newRed]<W[b][near[b]]){
-                    near[b]=newRed;
-                }
-            }
+        for (int i = 1; i < n; i++) {
+            isBlue[i] = true;
+            near[i] = 0;
         }
 
+        int newRed = 0;
 
+        for (int i = 1; i < n; i++) {
+            int minVal = INF;
+            for (int b = 0; b < n; b++) {
+                if(isBlue[b] && W[b][near[b]] < minVal) {
+                    minVal = W[b][near[b]];
+                    newRed = b;
+                }
+            }
+            isBlue[newRed] = false;
+            System.out.println((char)(near[newRed] + 'a') + " - " + (char)(newRed + 'a') + " : " + minVal);
+
+            for (int b = 0; b < n; b++) {
+                if(isBlue[b] && W[b][newRed] < W[b][near[b]]){
+                    near[b] = newRed;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        PrimeMST pm = new PrimeMST();
+
+        int[][] G = {
+                {0, 2, INF, INF, 4},
+                {2, 0, 8, INF, 4},
+                {INF, 8, 0, 7, 6},
+                {INF, INF, 7, 0, 3},
+                {4, 4, 6, 3, 0}
+        };
+
+        pm.mst(G);
+
+        Edge e1 = new Edge();
+        e1.setV1(1);
+        e1.setV2(2);
+        e1.setWeight(5);
+
+        List<Edge> edgeList = new ArrayList<>();
+
+        edgeList.add(e1);
+
+        for(Edge edge : edgeList){
+            edge.getWeight();
+        }
 
     }
 }
